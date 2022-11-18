721e58b8baf201c1648ae7198b85e9c745a02ce2
Sergey <16-@bk.ru>
2022-11-18 15:30:30 +0300

    fix bugs game

--- brain_games/code_parity_check.py
+++ brain_games/code_parity_check.py
@@ -3,20 +3,25 @@ import prompt
 from brain_games.cli import welcome_user
 
 
+def f_answer():
+    number = randint(1, 100)
+    answer = ''
+    if number % 2 == 0:
+        answer += 'yes'
+    if number % 2 != 0:
+        answer += 'no'
+    return number, answer
+
+
 def parity_check():
     name = welcome_user()   # import greeting
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    correct = 1     # keep score
+    correct = 1  # keep score
     while correct <= 3:     # cycle up to 3
-        number = randint(1, 100)
+        number, answer = f_answer()
         print(f'Question: {number}')    # give a number
         answer_user = prompt.string('Your answer: ')    # we get an answer
-        answer = ''
-        # checking the answer
-        if number % 2 == 0:
-            answer += 'yes'
-        if number % 2 != 0:
-            answer += 'no'
+        # checking the user answer
         if str(answer_user) == str(answer):
             print('Correct!')
             correct += 1
