6bfcd83fdbf01160586236060a4852c63b5fd1f3
Sergey <16-@bk.ru>
2022-11-19 12:40:41 +0300

    add main

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -8,9 +8,9 @@ from brain_games.logic_game import run_game
 from brain_games.games import code_progression
 
 
-def progression():
+def main():
     run_game(code_progression)
 
 
 if __name__ == '__main__':
-    progression()
+    main()
