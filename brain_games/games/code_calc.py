0892e5f045d1dc858742063579edc3ac50c0191f
Sergey <16-@bk.ru>
2022-11-22 20:34:48 +0300

    add operator to calc

--- brain_games/games/code_calc.py
+++ brain_games/games/code_calc.py
@@ -1,16 +1,19 @@
 import random
+import operator
 game_name = 'What is the result of the expression?'
 
 
 def generate_responses():
     number_1 = random.randint(1, 100)
     number_2 = random.randint(1, 100)
-    operator = random.choice(['-', '+', '*'])
-    if operator == '-':
-        result = number_1 - number_2
-    elif operator == '+':
-        result = number_1 + number_2
-    elif operator == '*':
-        result = number_1 * number_2
-    question = f'{number_1} {operator} {number_2}'
+    result = random.choice([operator.add(number_1, number_2),
+                            operator.mul(number_1, number_2),
+                            operator.sub(number_1, number_2)])
+    question = ''
+    if result == operator.add(number_1, number_2):
+        question = f'{number_1} + {number_2}'
+    elif result == operator.mul(number_1, number_2):
+        question = f'{number_1} * {number_2}'
+    elif result == operator.sub(number_1, number_2):
+        question = f'{number_1} - {number_2}'
     return question, result
