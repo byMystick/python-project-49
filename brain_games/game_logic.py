f093ae7e80cce4c6d5ae11221faf56e59a17b497
Sergey <16-@bk.ru>
2022-11-23 13:38:02 +0300

    changed game_logic

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -3,22 +3,17 @@ import prompt
 
 
 def run_game(module):
-    score = 0
     final_score = 3
     name = welcome_user()
     print(module.game_name)
-    while score <= final_score:
+    for i in range(final_score):
         question, answer = module.generate_responses()
         print(f'Question: {question}')
         answer_user = prompt.string('Your answer: ')
         if str(answer_user) == str(answer):
-            score += 1
             print('Correct!')
         else:
             print(f"'{answer_user}' is wrong answer ;(. "
                   f" Correct answer was '{answer}'."
                   f"\nLet's try again, {name}!")
-            break
-        if score == final_score:
-            print(f'Congratulations, {name}!')
-            break
+    print(f'Congratulations, {name}!')
