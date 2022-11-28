ebb54db9eaf81459a5fed81069f8d4d3823afea4
Sergey <16-@bk.ru>
2022-11-28 21:10:59 +0300

    fix logick

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -1,21 +1,21 @@
 from brain_games.cli import welcome_user
 import prompt
-
+score = 0
 
 def run_game(module):
     name = welcome_user()
     print(module.game_name)
-    for i in range(3):
+    for score in range(3):
         question, answer = module.generate_responses()
         print(f'Question: {question}')
         answer_user = prompt.string('Your answer: ')
         if str(answer_user) == str(answer):
             print('Correct!')
-            i += 1
+            score += 1
         else:
             print(f"'{answer_user}' is wrong answer ;(. "
                   f" Correct answer was '{answer}'."
                   f"\nLet's try again, {name}!")
             break
-    if i == 3:
+    if score == 3:
         print(f'Congratulations, {name}!')
